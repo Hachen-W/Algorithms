830fc8e51e000259392d48eac9c8975acd34c552
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-08 01:51:59 +0300

    optimize solving

--- LeetCode/task3/main.py
+++ LeetCode/task3/main.py
@@ -1,19 +1,14 @@
 class Solution:
-    def hasDuplicate(self, s: str) -> bool:
-        used_symbols = set()
-        for symbol in s:
-            if symbol in used_symbols:
-                return True
-            used_symbols.add(symbol)
-        return False
-
     def lengthOfLongestSubstring(self, s: str) -> int:
-        length = len(s)
-        answer = [0, 0]
-        for start in range(length):
-            for finish in range(start, length):
-                temp = s[start: finish + 1]
-                if not self.hasDuplicate(temp) and len(temp) > answer[1]:
-                    answer[0] = temp
-                    answer[1] = len(temp)
-        return answer[1]
+        current_string = ""
+        answer = 0
+
+        for symbol in s:
+            if symbol in current_string:
+                current_string = current_string.split(symbol)[1] + symbol
+            else:
+                current_string += symbol
+            if answer < len(current_string):
+                answer = len(current_string)
+
+        return answer
