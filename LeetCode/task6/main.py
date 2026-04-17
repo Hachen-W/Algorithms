f10b2f0f95a5d8bcae5bf2dcc2e30023689fe2b6
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-17 03:07:25 +0300

    change idea for task6

--- LeetCode/task6/main.py
+++ LeetCode/task6/main.py
@@ -3,25 +3,17 @@ import math
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        subarray_size = numRows * 2 - 2
-        string_length = len(s)
-        subarray_count = math.ceil(string_length / subarray_size)
-        subarray_index = 0
-        answer = [[] for _ in range(subarray_count)]
-        index = 0
-        while index < string_length:
-            if index % subarray_count == 0 or \
-                    index % subarray_count == subarray_count - 1:
-                answer[subarray_index].append(s[index])
-                subarray_index += 1
-                subarray_index = subarray_index % subarray_count
-            else:
-                answer[subarray_index].append(s[index])
-                answer[subarray_index].append(s[index + 1])
-                subarray_index += 1
-                index += 1
-            index += 1
-        return answer
+        stringLength = len(s)
+        zigzagFullCount = math.floor(string_length / subarray_size)
+        zigzagWidth = numRows - 1
+        zigzagLastWidth = stringLength % (numRows * 2 - 2)
+        if zigzagLastWidth == 0:
+            zigzagLastWidth = 0
+        elif zigzagLastWidth <= numRows:
+            zigzagLastWidth = 1
+        else:
+            zigzagLastWidth = 1 + zigzagLastWidth - numRows
+        answer = [[0 * numRows] for _ in range(zigzagWidth * zigzagFullCount + zigzagLastWidth)]
 
 
 print(Solution().convert("PAYPALISHIRING", 3))
