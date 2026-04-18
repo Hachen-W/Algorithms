094de9167f2b5b869b9646e6906af359cc40c699
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-18 20:34:26 +0300

    solve task6

--- LeetCode/task6/main.py
+++ LeetCode/task6/main.py
@@ -3,29 +3,34 @@ import math
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        stringLength = len(s)
-        zigzagFullCount = math.floor(string_length / subarray_size)
-        zigzagWidth = numRows - 1
-        zigzagLastWidth = stringLength % (numRows * 2 - 2)
-        if zigzagLastWidth == 0:
-            zigzagLastWidth = 0
-        elif zigzagLastWidth <= numRows:
-            zigzagLastWidth = 1
-        else:
-            zigzagLastWidth = 1 + zigzagLastWidth - numRows
-        numCols = zigzagWidth * zigzagFullCount + zigzagLastWidth
-        answer = [[0 * numRows] for _ in range(numCols)]
+        if numRows == 1:
+            return s
+        if numRows == 2:
+            return s[::2] + s[1::2]
+        string_length = len(s)
+        zigzag_symbols = numRows * 2 - 2
+        zigzag_count = math.ceil(string_length / zigzag_symbols)
+        answer = [[0] * zigzag_count * 2 for _ in range(numRows)]
 
-        offset = zigzagWidth
-        index = 0
-        for curRow in range(numRows):
-            for curCol in range(0, math.ceil(string_length / subarray_size) + 1, zigzagWidth):
-                if curCol - offset > 0 and offset != zigzagWidth and offset != 0:
-                    answer[curCol][curRow] = s[index]
-                    index += 1
-                answer[curCol][curRow] = s[index]
-                index += 1
-                offset -= 1
+        row_index = 0
+        column_index = 0
+        direction = 1
+        for zigzag_symbol in range(string_length):
+            answer[row_index][column_index] = s[zigzag_symbol]
+            if row_index == numRows - 1 or (row_index == 1 and direction == -1):
+                direction = direction * (-1)
+                column_index += 1
+                if row_index == 1 and direction == 1:
+                    row_index = 0
+                    continue
+            row_index += direction
+
+        answer_str = ''
+        for cur_row in range(numRows):
+            for cur_col in range(zigzag_count * 2):
+                if answer[cur_row][cur_col] != 0:
+                    answer_str += answer[cur_row][cur_col]
+        return answer_str
 
 
-print(Solution().convert("PAYPALISHIRING", 3))
+print(Solution().convert("ABCDE", 2))
