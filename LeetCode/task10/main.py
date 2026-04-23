3354b0bd599a95f5152bfc96590ca5d0dbbd99a6
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-24 02:44:00 +0300

    solve task10

--- LeetCode/task10/main.py
+++ LeetCode/task10/main.py
@@ -1,28 +1,18 @@
 class Solution:
-    def isMatch(self, string: str, pattern: str) -> bool:
-        string_index = 0
-        string_length = len(string)
-        pattern_length = len(pattern)
-        for pattern_index in range(pattern_length):
-            if pattern[pattern_index] == '*':
-                continue
-            elif string_index == string_length:
-                return False
-            elif pattern[pattern_index] == '.':
-                if pattern_index >= string_length:
-                    return False
-                if pattern_index + 1 < pattern_length and pattern[pattern_index + 1] == '*':
-                    return True
-            elif pattern_index + 1 != pattern_length and pattern[pattern_index + 1] == '*':
-                while string_index < string_length and string[string_index] == pattern[pattern_index]:
-                    string_index += 1
-                continue
-            elif pattern[pattern_index] != string[string_index]:
-                return False
-            string_index += 1
-        if string_index != string_length:
-            return False
-        return True
+    def isMatch(self, text: str, pattern: str) -> bool:
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (
+                self.isMatch(text, pattern[2:])
+                or first_match
+                and self.isMatch(text[1:], pattern)
+                )
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
 
 
 print(Solution().isMatch('mississippi', 'mis*is*p*.'))
