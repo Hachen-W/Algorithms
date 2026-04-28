0fbdf4b5e9dffb8b5fb872fa40f95285f8a1afd6
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-28 23:24:01 +0300

    solve task01

---
+++ Intern/T-Bank/summer_2026/task01.py
@@ -0,0 +1,11 @@
+def solution():
+    n = int(input())
+    if n % 2 == 0:
+        answer = "Anya"
+    else:
+        answer = "Masha"
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution())
