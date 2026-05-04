ede9fa3c1721589f6c0c1e18e224c0de224610d5
Hachen-W <mr.laiker.ru@gmail.com>
2026-05-04 04:07:44 +0300

    optimize task05

--- Intern/Yandex/Summer_School_2026/task05.py
+++ Intern/Yandex/Summer_School_2026/task05.py
@@ -1,72 +1,89 @@
 import sys
 from bisect import bisect_right
 
+
 def solution():
     input_data = sys.stdin.read().split()
     if not input_data:
         return
 
     n = int(input_data[0])
-
-    h = [0] * (n + 1)
-    for i in range(1, n + 1):
-        h[i] = int(input_data[i])
+    h = [int(x) for x in input_data[1:n+1]]
 
     A = [0] * (n + 1)
     B = [0] * (n + 1)
     
     vals_set = set()
-    for j in range(1, n + 1):
-        A[j] = h[j] - j
-        B[j] = h[j] + j
-        vals_set.add(A[j])
-        vals_set.add(B[j])
+    for j in range(n):
+        val_h = h[j]
+        aj = val_h - (j + 1)
+        bj = val_h + (j + 1)
+        A[j + 1] = aj
+        B[j + 1] = bj
+        vals_set.add(aj)
+        vals_set.add(bj)
 
     vals = sorted(list(vals_set))
     K = len(vals)
+
+    val_to_idx = {val: i for i, val in enumerate(vals, 1)}
+
+    idx_A = [0] * (n + 1)
+    idx_B = [0] * (n + 1)
+    for j in range(1, n + 1):
+        idx_A[j] = val_to_idx[A[j]]
+        idx_B[j] = val_to_idx[B[j]]
 
     BIT_A_count = [0] * (K + 1)
     BIT_A_sum = [0] * (K + 1)
     BIT_B_count = [0] * (K + 1)
     BIT_B_sum = [0] * (K + 1)
 
-    total_A_count = total_A_sum = 0
-    total_B_count = total_B_sum = 0
+    total_A_count = 0
+    total_A_sum = 0
+    total_B_count = n
+    total_B_sum = 0
 
     for j in range(1, n + 1):
-        idx = bisect_right(vals, B[j])
-        i_bit = idx
-        while i_bit <= K:
-            BIT_B_count[i_bit] += 1
-            BIT_B_sum[i_bit] += B[j]
-            i_bit += i_bit & (-i_bit)
-        total_B_count += 1
+        idx = idx_B[j]
+        BIT_B_count[idx] += 1
+        BIT_B_sum[idx] += B[j]
         total_B_sum += B[j]
 
-    ans = float('inf')
+    for i in range(1, K + 1):
+        nxt = i + (i & (-i))
+        if nxt <= K:
+            BIT_B_count[nxt] += BIT_B_count[i]
+            BIT_B_sum[nxt] += BIT_B_sum[i]
+
+    ans = 10**18
     target = n // 2 + 1
+    bs_right = bisect_right
+
+    max_h = max(h) if h else 0
+    max_possible_x = max_h + n
 
     for i in range(1, n + 1):
-        idx_b = bisect_right(vals, B[i])
-        i_bit = idx_b
-        while i_bit <= K:
-            BIT_B_count[i_bit] -= 1
-            BIT_B_sum[i_bit] -= B[i]
-            i_bit += i_bit & (-i_bit)
+        val_B = B[i]
+        idx_b = idx_B[i]
+        while idx_b <= K:
+            BIT_B_count[idx_b] -= 1
+            BIT_B_sum[idx_b] -= val_B
+            idx_b += idx_b & (-idx_b)
         total_B_count -= 1
-        total_B_sum -= B[i]
+        total_B_sum -= val_B
 
-        idx_a = bisect_right(vals, A[i])
-        i_bit = idx_a
-        while i_bit <= K:
-            BIT_A_count[i_bit] += 1
-            BIT_A_sum[i_bit] += A[i]
-            i_bit += i_bit & (-i_bit)
+        val_A = A[i]
+        idx_a = idx_A[i]
+        while idx_a <= K:
+            BIT_A_count[idx_a] += 1
+            BIT_A_sum[idx_a] += val_A
+            idx_a += idx_a & (-idx_a)
         total_A_count += 1
-        total_A_sum += A[i]
+        total_A_sum += val_A
 
-        low = 1
-        high = 2000000000
+        low = 0
+        high = max_possible_x
         x_opt = high
         
         while low <= high:
@@ -74,19 +91,17 @@ def solution():
             u = mid - i
             w = mid + i
             
-            idx_u = bisect_right(vals, u)
             c_A = 0
-            i_bit = idx_u
-            while i_bit > 0:
-                c_A += BIT_A_count[i_bit]
-                i_bit -= i_bit & (-i_bit)
+            idx_u = bs_right(vals, u)
+            while idx_u > 0:
+                c_A += BIT_A_count[idx_u]
+                idx_u -= idx_u & (-idx_u)
                 
-            idx_w = bisect_right(vals, w)
             c_B = 0
-            i_bit = idx_w
-            while i_bit > 0:
-                c_B += BIT_B_count[i_bit]
-                i_bit -= i_bit & (-i_bit)
+            idx_w = bs_right(vals, w)
+            while idx_w > 0:
+                c_B += BIT_B_count[idx_w]
+                idx_w -= idx_w & (-idx_w)
                 
             if c_A + c_B >= target:
                 x_opt = mid
@@ -100,25 +115,25 @@ def solution():
         u = X - i
         w = X + i
 
-        idx_u = bisect_right(vals, u)
-        cA_le = sA_le = 0
-        i_bit = idx_u
-        while i_bit > 0:
-            cA_le += BIT_A_count[i_bit]
-            sA_le += BIT_A_sum[i_bit]
-            i_bit -= i_bit & (-i_bit)
+        cA_le = 0
+        sA_le = 0
+        idx_u = bs_right(vals, u)
+        while idx_u > 0:
+            cA_le += BIT_A_count[idx_u]
+            sA_le += BIT_A_sum[idx_u]
+            idx_u -= idx_u & (-idx_u)
             
         cA_gt = total_A_count - cA_le
         sA_gt = total_A_sum - sA_le
         cost_A = (u * cA_le - sA_le) + (sA_gt - u * cA_gt)
 
-        idx_w = bisect_right(vals, w)
-        cB_le = sB_le = 0
-        i_bit = idx_w
-        while i_bit > 0:
-            cB_le += BIT_B_count[i_bit]
-            sB_le += BIT_B_sum[i_bit]
-            i_bit -= i_bit & (-i_bit)
+        cB_le = 0
+        sB_le = 0
+        idx_w = bs_right(vals, w)
+        while idx_w > 0:
+            cB_le += BIT_B_count[idx_w]
+            sB_le += BIT_B_sum[idx_w]
+            idx_w -= idx_w & (-idx_w)
             
         cB_gt = total_B_count - cB_le
         sB_gt = total_B_sum - sB_le
@@ -129,6 +144,7 @@ def solution():
             ans = total_cost
 
     print(ans)
+
 
 if __name__ == '__main__':
     solution()
